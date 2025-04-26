6dfa90081d3cddb1d23de9df520cda9caa52e6b9
世间微斯人 <3301601278@qq.com>
2025-04-26 13:57:00 +0800

    图标

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
+#include <QIcon>
+#include <QDir>
+#include <QFile>
+#include <QMessageBox>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    app.setWindowIcon(QIcon(":/favicon.ico"));
     
     MainWindow window;
     window.show();
